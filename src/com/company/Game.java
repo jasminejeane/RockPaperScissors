91add4688c6641ab9540bc56cefe5f34bd492ee0
jasminejeane <jasminejeane@gmail.com>
2018-05-07 09:08:49 -0700

    both players work

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -25,33 +25,27 @@ public class Game extends Items {
 
     public void play(){
         Scanner scan = new Scanner(System.in);
-//        System.out.println("Enter 1 for playing against computer or Enter 2 for two player game");
         System.out.println("Enter 1 for playing against computer or Enter 2 for two player game");
-
         playerType = scan.nextInt();
-
 
         if(playerType == 1){
             player1Choice = player1.getChoice();player2Choice = computer.getChoice();
-        }else if(playerType == 2) {
+        }else if(playerType == 2){
             player1Choice = player1.getChoice();
-        } else{
-            System.out.println("Please enter 1 or 2");
-            play();
+            player2Choice = player2.getChoice();
         }
 
         result = getResults();
-        results();
+        displayResults();
         stats();
 
     }
 
     public void viewHistory(){
-        System.out.println("Played: "+ (wins + loses + ties));
-        System.out.println("Won: " + wins);
-        System.out.println("Lost: " + loses);
-        System.out.println("Ties:" + ties);
-        System.out.println("Thanks for playing");
+        System.out.println("You have played "+ (wins + loses + ties));
+        System.out.println("You have won " + wins + " times");
+        System.out.println("You have lost " + loses + " times");
+        System.out.println("There were " + ties + " ties ..... Thanks for playing!");
 
     }
 
@@ -65,7 +59,7 @@ public class Game extends Items {
         }
     }
 
-    private void results(){
+    private void displayResults(){
         switch (result){
             case WIN:
                 System.out.println(player1Choice + " beats " + player2Choice + ". Player 1 Wins");
@@ -84,15 +78,15 @@ public class Game extends Items {
             return RESULT.TIE;
 
         }
-            switch (player1Choice){
-                case ROCK:
-                    return (player2Choice == CHOICES.SCISSORS ? RESULT.WIN : RESULT.LOSE);
-                case PAPER:
-                    return (player2Choice == CHOICES.ROCK ? RESULT.WIN : RESULT.LOSE);
-                case SCISSORS:
-                    return (player2Choice == CHOICES.PAPER ? RESULT.WIN : RESULT.LOSE);
+        switch (player1Choice){
+            case ROCK:
+                return (player2Choice == CHOICES.SCISSORS ? RESULT.WIN : RESULT.LOSE);
+            case PAPER:
+                return (player2Choice == CHOICES.ROCK ? RESULT.WIN : RESULT.LOSE);
+            case SCISSORS:
+                return (player2Choice == CHOICES.PAPER ? RESULT.WIN : RESULT.LOSE);
 
-            }
+        }
 
         return RESULT.LOSE;
     }
