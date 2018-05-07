af1a3a00216b421bc5e7fb87a82aeeaac2ec17e2
jasminejeane <jasminejeane@gmail.com>
2018-05-07 00:51:14 -0700

    game class

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -9,6 +9,32 @@ public class Game extends GameItems {
 
     public Game(){
         super();
+        player = new Player();
+        computer = new Computer();
+
+    }
+
+    public void play(){
+        playerChoice = player.getChoice();
+        computerChoice = computer.getChoice();
+
+    }
+
+    public RESULT getResult(){
+        if(playerChoice == computerChoice){
+            return RESULT.TIE;
+
+            switch (playerChoice){
+                case ROCK:
+                    return (computerChoice == CHOICES.SCISSORS ? RESULT.WIN : RESULT.LOSE);
+                case PAPER:
+                    return (computerChoice == CHOICES.ROCK ? RESULT.WIN : RESULT.LOSE);
+                case SCISSORS:
+                    return (computerChoice == CHOICES.PAPER ? RESULT.WIN : RESULT.LOSE);
+
+            }
+        }
+        return RESULT.LOSE;
     }
 
 }
